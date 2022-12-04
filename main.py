b267c0525ca23a296932d02ead59875fd97885d3
Hector Robertson <hectormrobertson@gmail.com>
2022-12-04 17:23:06 +0000

    Start

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+__version__ = "Prototype 0.0.1 (git-main)"
+
+print("Welcome to Make-A-Game" + __version__)
+
+root = tk.Tk()
+root.title("Make-a-game")
+root.geometry("500x500")
+
+root.mainloop()
